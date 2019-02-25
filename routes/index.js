7b4be6060e54110894f67a1660eb839b88c97d29
Danilov Igor <danilovigor773@gmail.com>
2019-02-25 11:24:51 +0500

    Created examples for understanding pug basics

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,20 @@ const router = express.Router();
 // Do work here
 router.get('/', (req, res) => {
   //console.log('Hey!');
-  const wes = {name: 'Wes', age: 100, cool: true};
+  //const wes = {name: 'Wes', age: 100, cool: true};
   //res.send('Hey! It works!');
   //res.json(wes);
-  res.send(req.query);
+  // res.send(req.query);
+  res.render('hello', {
+    name: 'Igor',
+    cat: req.query.cat,
+    title: 'Come ON!'
+  });
 });
 
 router.get('/reverse/:name', (req, res) => {
   const reverse = [...req.params.name].reverse().join('');
   res.send(reverse);
-
 });
 
 module.exports = router;

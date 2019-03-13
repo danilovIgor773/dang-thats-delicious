5c8a3253ced573ea727eb5391cef5b5d8d9d3e97
Danilov Igor <danilovigor773@gmail.com>
2019-03-13 16:08:00 +0500

    Created functionality for updating and editing the 'Stores'

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -22,3 +22,25 @@ exports.getStores = async (req, res) => {
   //console.log(stores);
   res.render('stores', {title: 'Stores', stores: stores});
 };
+
+exports.editStore = async (req, res) => {
+  //1. Find the store given the ID
+  const store = await Store.findOne({ _id: req.params.id });
+  //2. confirm the user is the owner of the store
+  //3. Render out the edit form so the user can update their store
+  res.render('editStore', {title: `Edit ${store.name}`, store: store});
+
+};
+
+exports.updateStore = async (req, res) => {
+  // find and update stores
+  const store = await Store.findOneAndUpdate({ _id: req.params.id}, req.body,
+    {
+    new: true, // return the new store instead of the old one
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>.
+      <a href="/stores/${store.slug}">View store -></a>`);
+  //Redirect them the store and tell them it worked
+  res.redirect(`/stores/${store._id}/edit`);
+};
